ea4fd3f4a405d8b2582e19690c2283771f6d7681
Nowaaru <nowaaru@proton.me>
2024-04-03 17:19:21 -0700

    line endings, probably

--- src/client/controllers/combat.ts
+++ src/client/controllers/combat.ts
@@ -4,26 +4,26 @@ import { OnKeyboardInput } from "@rbxts/quarrelgame-framework/out/client/control
 import { PlatformCameraController2D } from "client/controllers/platformcamera2d";
 
 @Controller({})
-export class Combat extends CombatController2D implements OnRespawn, OnInit, OnKeyboardInput
-{
-    constructor()
-    {
+export class Combat extends CombatController2D implements OnRespawn, OnInit, OnKeyboardInput {
+    constructor() {
         super(Dependency<MotionInput.MotionInputController>(), Dependency<PlatformCameraController2D>());
 
         this.keybindMap = new Map<Enum.KeyCode, Input>([
-            [ Enum.KeyCode.Seven, Input.Roman ],
-            [ Enum.KeyCode.Nine, Input.Burst ],
-            [ Enum.KeyCode.U, Input.Slash ],
-            [ Enum.KeyCode.I, Input.Punch ],
-            [ Enum.KeyCode.O, Input.Kick ],
-            [ Enum.KeyCode.J, Input.Heavy ],
-            [ Enum.KeyCode.K, Input.Dust ],
-            [ Enum.KeyCode.L, Input.Sweep ],
+            [Enum.KeyCode.Seven, Input.Roman],
+            [Enum.KeyCode.Nine, Input.Burst],
+            [Enum.KeyCode.U, Input.Slash],
+            [Enum.KeyCode.I, Input.Punch],
+            [Enum.KeyCode.O, Input.Kick],
+            [Enum.KeyCode.J, Input.Heavy],
+            [Enum.KeyCode.K, Input.Dust],
+            [Enum.KeyCode.L, Input.Sweep],
         ]);
     }
 
-    onRespawn(character: Model): void
-    {
+    onInit(): void | Promise<void> {
+    }
+
+    onRespawn(character: Model): void {
         super.onMatchRespawn(character);
         print("on respawn!!");
     }
