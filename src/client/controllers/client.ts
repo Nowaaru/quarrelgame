352e9012f56823f2b1eaf604e354dbc5434ab311
nowaaru@proton.me <vert@leanbox.>
2024-09-01 11:01:12 +0000

    formatting i think
    also add the characters to the animator you should do that
    
    Signed-off-by: nowaaru@proton.me <vert@leanbox.>

--- src/client/controllers/client.ts
+++ src/client/controllers/client.ts
@@ -1,29 +1,60 @@
-import { Controller, Dependency, OnInit } from "@flamework/core";
+import { Controller, Dependency, OnInit, OnStart } from "@flamework/core";
 import { Players, StarterGui } from "@rbxts/services";
 
-import { Client as QGFClient, CharacterSelectController } from "@quarrelgame-framework/client";
+import {
+    Client as QGFClient,
+    CharacterSelectController,
+    OnCharacterSelected,
+} from "@quarrelgame-framework/client";
 import { CommandController } from "./command";
 import Characters from "data/character";
+import { Animator, Character } from "@quarrelgame-framework/common";
+import { Functions, QuarrelFunctions } from "client/network";
 
-print("ruh roh raggy");
-export class Client extends QGFClient implements OnInit
+@Controller({})
+export class Client
+    extends QGFClient
+    implements OnStart, OnInit, OnCharacterSelected 
 {
-    constructor(protected characterSelectController: CharacterSelectController, protected commandController: CommandController)
-    { super(); }
+    constructor(
+        protected characterSelectController: CharacterSelectController,
+        protected commandController: CommandController,
+    ) {
+        super();
+    }
 
-    onInit(): void {
-        print("i'm gonna blow up and act like i don't know nbobody!!")
+    onInit(): void 
+    {
+        super.onInit();
+        print("i'm gonna blow up and act like i don't know nbobody!!");
 
         Players.LocalPlayer.CameraMinZoomDistance = 8;
-        Players.LocalPlayer.CameraMaxZoomDistance = Players.LocalPlayer.CameraMinZoomDistance;
+        Players.LocalPlayer.CameraMaxZoomDistance =
+            Players.LocalPlayer.CameraMinZoomDistance;
 
         StarterGui.SetCoreGuiEnabled(Enum.CoreGuiType.All, false);
         this.commandController.SetKeys(Enum.KeyCode.Backquote, Enum.KeyCode.F2);
+
+        Animator.RegisterCharacters(Characters);
     }
 
-    onStart()
+    onStart() 
     {
         print("Example Client loaded.");
         this.characterSelectController.SetCharacters(Characters);
+    }
+
+    onCharacterSelected(character: Character.Character): void 
+    {
+        QuarrelFunctions.SelectCharacter(character.Name).then(async () =>
+            QuarrelFunctions.Ready().then(() =>
+                QuarrelFunctions.MatchTest()
+                    .then(() => {
+                        print("oh my goodness gracious lol");
+                        return;
+                    })
+                    .finally(() => this.characterSelectController.CloseCharacterSelect()),
+            ),
+        );
     }
 }
