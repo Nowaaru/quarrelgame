c711f31abeec46f1da63a053ff6e0e3d07c1ce12
nowaaru@proton.me <vert@leanbox.>
2024-09-01 10:59:46 +0000

    update paths
    Signed-off-by: nowaaru@proton.me <vert@leanbox.>

--- src/client/runtime.client.ts
+++ src/client/runtime.client.ts
@@ -1,7 +1,10 @@
 import { Flamework, Modding } from "@flamework/core";
 import { MatchController } from "@quarrelgame-framework/client";
 
-Flamework.addPaths("node_modules/@quarrelgame-framework/client/controllers");
+// Flamework.addPaths("node_modules/@quarrelgame-framework/client/out/controllers");
+Flamework.addPaths("node_modules/@quarrelgame-framework/client");
+Flamework.addPaths("node_modules/@quarrelgame-framework/common");
+
 Flamework.addPaths("src/client/controllers");
 Flamework.addPaths("src/client/components");
 Flamework.addPaths("src/shared/components");
