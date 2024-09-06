359343bbdc478182e0509a6ed2d9023a2b4ad61a
nowaaru@proton.me <vert@leanbox.>
2024-09-06 12:51:25 +0000

    test decorator-based skill registration
    yeah it works frickin' amazingly well!!! :D:D:D:D:D:D
    
    Signed-off-by: nowaaru@proton.me <vert@leanbox.>

---
+++ src/data/character/death/normals/2H.ts
@@ -0,0 +1,47 @@
+import { Animation, QGSkill, Skill, Hitbox, MotionInput, SkillLike } from "@quarrelgame-framework/common";
+
+// export const LowHeavy = new Skill.SkillBuilder()
+//     .SetName("Low Heavy Slash")
+//     .SetGroundedType(Skill.SkillGroundedType.Ground)
+//     .SetFrameData(
+//     )
+//     .Construct();
+
+@QGSkill({
+    id: "tgr2H"
+})
+export class LowHeavy extends Skill.Skill
+{
+    Name: string = "Low Heavy Slash";
+    FrameData: Skill.FrameData = 
+        new Skill.FrameDataBuilder()
+            .SetStartup(11)
+            .SetActive(6)
+            .SetRecovery(21)
+            .SetContact(4)
+            .SetBlockAdvantage(8)
+            .SetHitbox(
+                new Hitbox.HitboxBuilder()
+                    .SetOffset(new Vector3(0, 0.5, 2))
+                    .SetSize(new Vector3(2, 2, 3.5))
+                    .Construct(),
+            )
+            .SetAnimation(
+                new Animation.AnimationBuilder()
+                    .IsAttack()
+                    .SetName("2HS")
+                    .SetAnimationId("rbxassetid://14322453534")
+                    .Construct(),
+            ).Construct();
+
+    IsReversal = false;
+    CanCounter = true;
+
+    GroundedType: Skill.SkillGroundedType = Skill.SkillGroundedType.Ground;
+    GaugeRequired = 0;
+
+    GatlingsInto: [MotionInput, SkillLike][] = [];
+    LinksInto?: SkillLike | undefined = undefined;
+}
+
+export default LowHeavy;
